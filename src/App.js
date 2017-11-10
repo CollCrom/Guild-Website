b5f099881b5cf59c317ea7a1159ba62bee4d01cb
Collin Cromwell <collcrom@gmail.com>
2017-11-10 13:58:21 -0600

    added nav bar component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Head from './Header/Header.js'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Head/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Header/Header.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Head extends Component {
+	render(){
+		return (
+			<div>
+				<div>
+					<a href='test'>Tribe of the Ascended</a>
+					<a href='test'>Roster</a>
+					<a href='test'>Contact</a>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Head;
