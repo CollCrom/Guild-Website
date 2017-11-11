5fb3f1c3263abdcae2f844d0491945eb77c7dc4c
Collin Cromwell <collcrom@gmail.com>
2017-11-11 16:31:09 -0600

    got react router to work

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
-import Head from './Header/Header.js'
-import Homepage from './Homepage/Homepage.js'
-import Contact from './Contact/Contact.js'
-import Roster from './Roster/Roster.js'
+import Header from './Header/Header.js'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Head/>
-        <Homepage/>
-        <Roster/>
-        <Contact/>
+        <Header/>
       </div>
     );
   }
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,18 +1,27 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import Homepage from '../Homepage/Homepage.js'
+import Contact from '../Contact/Contact.js'
+import Roster from '../Roster/Roster.js'
 
-class Head extends Component {
+class Header extends Component {
 	render(){
 		return (
-			<div>
+			<Router>
 				<div>
-					<a href='test'>Tribe of the Ascended</a>
-					<a href='test'>Roster</a>
-					<a href='test'>Contact</a>
-				</div>
-			</div>
+					<ul>
+						<li><Link to="/">Tribe of the Ascended</Link></li>
+						<li><Link to="/roster">Roster</Link></li>
+						<li><Link to="/contact">Contact</Link></li>
+					</ul>
+
+					<Route exact path="/" component={Homepage}/>
+	      	<Route path="/roster" component={Roster}/>
+	      	<Route path="/contact" component={Contact}/>
+	      </div>
+			</Router>
 		);
 	}
 }
 
-export default Head;
+export default Header;
